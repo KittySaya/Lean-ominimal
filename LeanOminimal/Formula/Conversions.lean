65cd44ab017765330be1c6e9fd5fbe76058a7fc1
Lily <kittysaya2020@gmail.com>
2025-05-22 17:38:59 +0200

    Conversions Finalized?

--- LeanOminimal/Formula/Conversions.lean
+++ LeanOminimal/Formula/Conversions.lean
@@ -145,7 +145,7 @@ This lemma states that, for an existblock `eb`, calling `eb.toImpAllFreeFormulaW
 is the same as calling `eb.toImpAllFreeFormula`.
 -/
 @[simp]
-lemma toImpAllFreeFormula_equivalence {L} {α} {n} (eb : Existblock L α (n+1) ) : eb.toImpAllFreeFormula = eb.toQFImpAllFreeFormula.toImpAllFreeFormula.exists := rfl
+lemma toImpAllFreeFormula_equivalence {L} {α} {n} (eb : Existblock L α (n+1)) : eb.toImpAllFreeFormula = eb.toQFImpAllFreeFormula.toImpAllFreeFormula.exists := rfl
 
 end Existblock
 
@@ -282,41 +282,3 @@ lemma QFImpAllFree_Bounded_conversion_equivalence {L} {α} {n} (Rs : disjunction
 end disjunctionRelblocks
 
 ------------------------------------------------------
-
-/--
-Sends ImpAllFreeFormula `φ` in the language of `(ℝ, <)` to a QFImpAllFreeFormula that is equivalent to it.
--/
-def ImpAllFreeFormula.toQFImpAllFreeFormula  {n:ℕ } : ImpAllFreeFormula (order_language[[ℝ]]) (Fin 1) (n) → QFImpAllFreeFormula (order_language[[ℝ]]) (Fin 1) n :=
-  let rec helper {n} : ImpAllFreeFormula (order_language[[ℝ]]) (Fin 1) n →
-      disjunctionExistblocks (order_language[[ℝ]]) (Fin 1) n
-    | .falsum => disjunctionExistblocks.existbl (Existblock.lit (Literal.truth.not))
-    | .equal t1 t2 => disjunctionExistblocks.existbl (Existblock.lit (Literal.equal t1 t2))
-    | .rel R f => disjunctionExistblocks.existbl (Existblock.lit (Literal.rel R f))
-    | .and t1 t2=> (helper t1).and (helper t2)
-    | .or t1 t2=> (helper t1).or (helper t2)
-    | .not t => (helper t)
-    | .exists φ  => match φ with
-      | .falsum =>
-          disjunctionExistblocks.existbl (Existblock.lit (Literal.truth.not))
-      | .equal t1 t2 =>
-          (disjunctionExistblocks.existbl (Existblock.lit (Literal.equal t1 t2))).elim
-      | .rel R f =>
-          (disjunctionExistblocks.existbl (Existblock.lit (Literal.rel R f))).elim
-      | .not φ  =>
-          notExistblockelim (helper  φ )
-      | .or φ₁ φ₂ =>
-          (helper φ₁).elim.or (helper φ₂).elim
-      | .and φ₁ φ₂ =>
-          ((helper φ₁).and (helper φ₂)).elim
-      | .exists φ =>
-          (helper φ).elim.elim
-
-  fun φ =>
-      match φ with
-      | .falsum        => QFImpAllFreeFormula.falsum
-      | .equal t1 t2   => QFImpAllFreeFormula.equal t1 t2
-      | .rel R f       => QFImpAllFreeFormula.rel R f
-      | .not φ         => φ.toQFImpAllFreeFormula.not
-      | .or φ₁ φ₂      => φ₁.toQFImpAllFreeFormula.or φ₂.toQFImpAllFreeFormula
-      | .and φ₁ φ₂     => φ₁.toQFImpAllFreeFormula.and φ₂.toQFImpAllFreeFormula
-      | .exists φ      => (helper (.exists φ)).toQFImpAllFreeFormula
