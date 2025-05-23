ff541651518e606bbf08b53014ef8be045a70919
Lily_LittleAngel <kittysaya2020@gmail.com>
2025-05-23 15:26:35 +0200

    Added docstrings for Definability.Definition

--- LeanOminimal/Definability/Definition.lean
+++ LeanOminimal/Definability/Definition.lean
@@ -6,6 +6,11 @@ open Set
 
 namespace Definability
 
+/--
+A set `U` of type `X` in language `L` is definable if there
+exists some bounded formula `φ` in the language `L` with constants
+of `X` such that `U` is the set of all `X` satisfying `φ`.
+-/
 @[simp]
 def isDefinable {X : Type} (L : Language) (U : Set X) [Language.Structure L X] : Prop :=
   Definable₁ (⊤ : Set X ) L U
