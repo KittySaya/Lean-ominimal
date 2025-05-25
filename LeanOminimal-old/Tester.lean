0187265e7c1a3618db2959559a7cb22f072fbee8
Lily <kittysaya2020@gmail.com>
2025-05-25 14:30:26 +0200

    Deleted mmore empty fil;es

--- LeanOminimal-old/Tester.lean
+++
@@ -1,8 +0,0 @@
-import LeanOminimal.Basic
-
-def my_bad : sorry :=
-  sorry
-
-#check my_bad
-#check hello
-#eval hello
--- LeanOminimal-old/skeletonToBeMovedLily.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib
-import LeanOminimal.intervals
-import LeanOminimal.Definability.Definition
-import LeanOminimal.ominimality
-
-open FirstOrder
-section order_definition
